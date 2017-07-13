1152f02a1c6c819bde87dea56017d83eddab81a2
Alex Chalk <alexchalk17@gmail.com>
2017-07-13 15:16:30 -0400

    Create point object and method to shift its coordinates

--- src/clojure_embedded_oo/core.clj
+++ src/clojure_embedded_oo/core.clj
@@ -1,7 +1,24 @@
 (ns clojure-embedded-oo.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def Point
+  (fn [x y]
+    {:x x 
+     :y y
+     :__class_symbol__ 'Point}))
+
+;; The two below are equivalent
+(def y (fn [this] (:y this)))
+(def x :x)
+
+(def class-of :__class_symbol__)
+(def shift
+  (fn [this xinc yinc]
+    (Point (+ (x this) xinc)
+           (+ (y this) yinc))))
+
+
+(def first-object (Point 3 2))
+(x first-object)
+(shift first-object -1 -200)
+
