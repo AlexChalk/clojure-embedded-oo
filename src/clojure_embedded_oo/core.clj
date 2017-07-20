e2c5095ba33e763b525639afb8abc27ab29aac81
Alex Chalk <alexchalk17@gmail.com>
2017-07-20 14:48:07 -0400

    Refactor: DRY out code
    Move common behaviour of 'make' and 'send-to' into separate function.

--- src/clojure_embedded_oo/core.clj
+++ src/clojure_embedded_oo/core.clj
@@ -1,18 +1,20 @@
 (ns clojure-embedded-oo.core
  (:gen-class))
 
+(def apply-message-to 
+  (fn [class instance message args]
+    (let [method (message (:__instance_methods__ class))]
+      (apply method instance args))))
+                                                 
 (def make
-  (fn [class & args]
-    (let [seeded {:__class_symbol__ (:__own_symbol__ class)}
-          constructor (:add-instance-values
-                        (:__instance_methods__ class))]
-        (apply constructor seeded args))))
+ (fn [class & args]
+   (let [seeded {:__class_symbol__ (:__own_symbol__ class)}]
+    (apply-message-to class seeded :add-instance-values args))))
           
 (def send-to
   (fn [instance message & args]
-    (let [class (eval (:__class_symbol__ instance))
-          method (message (:__instance_methods__ class))]
-      (apply method instance args))))
+    (let [class (eval (:__class_symbol__ instance))]
+      (apply-message-to class instance message args))))
 
 (def Point
   {
